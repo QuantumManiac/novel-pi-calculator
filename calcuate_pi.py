d0cf3b6dd52ae62f47d144cbb82afa0904d0ff80
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2020-06-21 17:48:37 -0400

    Add docstrings to functions

--- calcuate_pi.py
+++ calcuate_pi.py
@@ -5,12 +5,22 @@ import time
 
 def calculate_pi(n, give_time=False, logging=False):
     """
-    Calculates the value of pi to n digits by simulating a system of two boxes and a wall colliding
+    Calculates the value of pi to n digits by simulating a system of two boxes and a wall colliding, and counting the
+    number of collisions between object 1 and object 2 or the wall before it can no longer collide with another body:
+
+    | Wall
+    |
+    |           |--------|      init_speed |--------|
+    |           |Object 1|     <=========  |Object 2|
+    |           |________|                 |________|
+    -------------------------------------------------
+
     :param n: digits of pi to calculate
     :param give_time: if True, prints the time taken in terminal following function call
     :param logging: if True, returns a list of tuples in format (collision#, obj1 vel, obj2, vel), appended after
            every collision
-    :return: default:
+    :return: default: the calculated value of pi
+             with logging=True: a tuple with the form (calculated_pi, log)
     """
     init_time = time.perf_counter()
     log = []
@@ -52,6 +62,13 @@ def calculate_pi(n, give_time=False, logging=False):
 
 
 def plot_results(log, show_polygon=True):
+    """
+    Plots the points from the given log file, with the velocity of object 1 as the y-axis and the velocity of object 2
+    as the x-axis.
+    :param log: logfile produced from calculate_pi to use
+    :param show_polygon: whether or not to plot the circle that the points make
+    :return: none
+    """
     collisions, obj1, obj2, = zip(*log)
 
     def modify_list(li):
@@ -70,7 +87,6 @@ def plot_results(log, show_polygon=True):
         plt.plot(poly_x, poly_y, label='Polygon', linewidth=2)
     plt.xlabel("Velocity of Object 2")
     plt.ylabel("Velocity of Object 1")
-    plt.legend()
 
     plt.show()
 
