1626a226af0d6c37269d250742dbe041ba1a3faa
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2020-06-21 16:49:36 -0400

    Add matplot plotting to log files for main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from calcuate_pi import *
+from matplotlib import pyplot as plt
 
 n = int(input('Enter the number of digits of pi to calculate (recommend < 8): '))
-print(f"Result: {calculate_pi(n, True, False)}")
+
+result, log = calculate_pi(n, True, True)
+
+print(f"Result: {result}")
 
 
+collisions, obj1, obj2, = zip(*log)
 
+plt.plot(collisions, obj1, label="Object 1")
+plt.plot(collisions, obj2, label="Object 2")
+
+plt.xlabel("# of collisions")
+plt.ylabel("velocity")
+plt.legend()
+
+plt.show()
 
 
